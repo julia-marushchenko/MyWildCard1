0cf09c15e92c2d4b69b3d80ad13ef68d3f6adcf7
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-05-18 20:18:23 +0200

    Add files via upload
    Committing Main class.

---
+++ src/com/mygenerics/Main.java
@@ -0,0 +1,34 @@
+/**
+ *  Java program to create own class with generic type.
+ */
+
+package com.mygenerics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *  Main class.
+ */
+public class Main {
+
+    // JVM entry point.
+    public static void main(String[] args) {
+
+        // Creating lists with different types.
+        ArrayList<Long> stringList = new ArrayList<>(Arrays.asList(11L, 12L, 13L));
+        ArrayList<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3));
+
+        // Calling method wildCardDemo() for two lists.
+        wildCardDemo(stringList);
+        wildCardDemo(integerList);
+    }
+
+    // Method to print elements of lists with wildcard type.
+    static void wildCardDemo(List<? extends Number> list) {
+        for (Object o : list) {
+            System.out.println("The item is: " + o);
+        }
+    }
+}
